0800ae1ea19f3b1e84dea0c47c1c18cf9449aa7f
Mert Çakar <mertcakar.es@gmail.com>
2023-05-30 00:31:24 +0300

    Initial commit.

---
+++ service/src/main/java/mc/budgetbuddy/BudgetBuddyApplication.java
@@ -0,0 +1,13 @@
+package mc.budgetbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBuddyApplication.class, args);
+	}
+
+}
